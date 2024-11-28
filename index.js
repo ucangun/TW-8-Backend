bd86b8929665d8308e5a5cae46fc4b9bdb975661
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-28 20:21:27 +0100

    "Added express-async-errors package and implemented it in index.js, updated package.json and package-lock.json accordingly."

---
+++ index.js
@@ -0,0 +1,34 @@
+/*****************************************************
+                    EXPRESS.JS 
+# npm init -y 
+# npm i express dotenv
+# npm i mongoose             
+# npm i express-async-errors
+/****************************************************/
+const connectDB = require("./src/configs/db");
+const express = require("express");
+const app = express();
+
+/****************************************************/
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+// asyncErrors to errorHandler:
+require("express-async-errors");
+
+// DB Connection
+connectDB();
+
+/****************************************************/
+
+// Middlewares:
+
+// Parses JSON request bodies.
+app.use(express.json());
+
+/****************************************************/
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://127.0.0.1:${PORT}`);
+});
